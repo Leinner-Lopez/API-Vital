66f5b5eeb075d9244021b8ad588a11f037d6ba97
Leinner-Lopez <leinner1006@gmail.com>
2026-01-16 13:06:15 -0500

    Cambios en el jwt Service para que se envie en el jwt en id y el rol del Usuario el cual esta iniciando sesión

--- src/main/java/com/vitalapi/Auth/AuthService.java
+++ src/main/java/com/vitalapi/Auth/AuthService.java
@@ -8,7 +8,6 @@ import com.vitalapi.Services.PacienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
--- src/main/java/com/vitalapi/Auth/JwtService.java
+++ src/main/java/com/vitalapi/Auth/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -28,6 +29,8 @@ public class JwtService {
         return Jwts
                 .builder()
                 .claims(claims)
+                .claim("UserId",usuario.getNumeroDocumento())
+                .claim("Role",usuario.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst().orElse(null))
                 .subject(usuario.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis()+1000*60*24))

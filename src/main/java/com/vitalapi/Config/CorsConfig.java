924ed719ce657865c0ebd63037e6040cc99d0602
Leinner-Lopez <leinner1006@gmail.com>
2026-01-21 20:44:22 -0500

    Configuración de los Cors Actualizada

--- src/main/java/com/vitalapi/Config/CorsConfig.java
+++ src/main/java/com/vitalapi/Config/CorsConfig.java
@@ -1,15 +1,18 @@
 package com.vitalapi.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }

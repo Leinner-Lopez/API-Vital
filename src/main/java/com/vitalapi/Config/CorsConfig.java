2ffe5c24aa68539b21f75edccef3ef60360692fc
Leinner-Lopez <leinner1006@gmail.com>
2026-01-28 15:08:27 -0500

    Se agrego el dominio que esta predestinado para el frontend, para que no haya problemas con las peticiones y se agrego el manejo de los cors en la configuracion de spring security

--- src/main/java/com/vitalapi/Config/CorsConfig.java
+++ src/main/java/com/vitalapi/Config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200","https://vital-care-six.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
